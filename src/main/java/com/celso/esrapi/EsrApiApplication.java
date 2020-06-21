8e002ee0da21f3d84825a57cd796a180480ef71d
Celso Bueno <cbtbueno@gmail.com>
2020-06-21 13:37:28 -0300

    Criação do Projeto

---
+++ src/main/java/com/celso/esrapi/EsrApiApplication.java
@@ -0,0 +1,13 @@
+package com.celso.esrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/celso/esrapi/EsrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celso.esrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
